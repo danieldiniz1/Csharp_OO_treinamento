ed7a1bb07be3e33e67c54599c36fcdf8f46a0064
Daniel Diniz <daniel.diniz1@icloud.com>
2021-11-02 20:03:52 -0300

    metodos de definir e obter saldo

---
+++ 06-Bank/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Bank
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string cpf { get; set; }
+        public string profissao { get; set; }
+    }
+}   
---
+++ 06-Bank/ContaCorrente.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Bank
+{
+    class ContaCorrente
+    {
+        public int id;
+        public Cliente titular;
+        public int agencia;
+        public int numeroConta;
+        private double saldo = 100;
+
+
+
+        public void DefinirSaldo(double saldo)
+        {
+            if (saldo < 0)
+            {
+
+            }
+            else
+            {
+                this.saldo = saldo;
+            }
+        }
+        public double ObterSaldo()
+        {
+            return saldo;
+        }
+        public bool Sacar(double valor)
+        {
+            if(saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                return true;
+            }
+        }
+
+        public void Depositar (double valor)
+        {
+            saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                saldo -= valor;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+        }
+    }
+}
